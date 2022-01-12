f3d05f72383deeea0336a4a97e2ec6816ff8b091
FebriTahta <febrirtah@gmail.com>
2022-01-12 08:50:24 +0700

    2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,13 @@
 <!-- Jquery Core Js --> 
-<script src="{{asset('assets/bundles/libscripts.bundle.js')}}"></script> <!-- Lib Scripts Plugin Js --> 
-<script src="{{asset('assets/bundles/vendorscripts.bundle.js')}}"></script> <!-- Lib Scripts Plugin Js --> 
+<script src="{{asset('assets/bundles/libscripts.bundle.js')}}"></script> <!-- Lib Scripts Plugin Js ( jquery.v3.2.1, Bootstrap4 js) -->
+<script src="{{asset('assets/bundles/vendorscripts.bundle.js')}}"></script> <!-- slimscroll, waves Scripts Plugin Js -->
+
+<script src="{{asset('assets/bundles/knob.bundle.js')}}"></script> <!-- Jquery Knob-->
 
 <script src="{{asset('assets/bundles/fullcalendarscripts.bundle.js')}}"></script><!--/ calender javascripts --> 
 
 <script src="{{asset('assets/bundles/mainscripts.bundle.js')}}"></script><!-- Custom Js --> 
-{{-- <script src="assets/js/pages/calendar/calendar.js"></script> --}}
+<script src="{{asset('assets/js/pages/calendar/calendar.js')}}"></script>
 {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" /> --}}
  
 <script src="https://cdn.jsdelivr.net/npm/moment@2.27.0/moment.min.js"></script>
