f990dbfc94a61bcd99f8c5efa67a20c43638973d
FebriTahta <febrirtah@gmail.com>
2022-01-18 12:58:35 +0700

    cronjob

--- app/Console/Commands/reminderjurnal.php
+++ app/Console/Commands/reminderjurnal.php
@@ -11,14 +11,14 @@ class reminderjurnal extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'reminder:cron';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Mengirimkan pesan whatsapp ke anggota yang belum mengisi jurnal';
 
     /**
      * Create a new command instance.
@@ -37,6 +37,33 @@ class reminderjurnal extends Command
      */
     public function handle()
     {
-        return 0;
+        // return 0;
+        $x = App\Models\Joblist::select('anggota_id')->whereDate('start', date('Y-m-d'))->distinct()->get();
+        $y = App\Models\Anggota::select('*')->whereNotIn('id',$x)->get();
+
+        foreach ($y as $key => $value) {
+            # code...
+            $curl = curl_init();
+                    $token = "dyr07JcBSmVsb1YrVBTB2A5zNKor0BZ9krv2WnQsjWHG1CRhSktdqazkfuOSY9qh";
+                    $datas = [
+                        'phone' => '081329146514',
+                        'message' => $value->nama.' - Jangan lupa mengisi jurnal kegiatan harian anda pada . *https://jurnal.tilawatipusat.com*',
+                        'secret' => false, // or true
+                        'priority' => false, // or true
+                    ];
+                    curl_setopt($curl, CURLOPT_HTTPHEADER,
+                        array(
+                            "Authorization: $token",
+                        )
+                    );
+                    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($datas));
+                    curl_setopt($curl, CURLOPT_URL, "https://simo.wablas.com/api/send-message");
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+                    $result = curl_exec($curl);
+                    curl_close($curl);
+        }
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -18,32 +18,32 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Artisan::command('reminderjurnal', function(){
-    $x = App\Models\Joblist::select('anggota_id')->whereDate('start', date('Y-m-d'))->distinct()->get();
-    $y = App\Models\Anggota::select('*')->whereNotIn('id',$x)->get();
+// Artisan::command('reminderjurnal', function(){
+//     $x = App\Models\Joblist::select('anggota_id')->whereDate('start', date('Y-m-d'))->distinct()->get();
+//     $y = App\Models\Anggota::select('*')->whereNotIn('id',$x)->get();
 
-    foreach ($y as $key => $value) {
-        # code...
-        $curl = curl_init();
-                $token = "dyr07JcBSmVsb1YrVBTB2A5zNKor0BZ9krv2WnQsjWHG1CRhSktdqazkfuOSY9qh";
-                $datas = [
-                    'phone' => '081329146514',
-                    'message' => $value->nama.' - Jangan lupa mengisi jurnal kegiatan harian anda pada . *https://jurnal.tilawatipusat.com*',
-                    'secret' => false, // or true
-                    'priority' => false, // or true
-                ];
-                curl_setopt($curl, CURLOPT_HTTPHEADER,
-                    array(
-                        "Authorization: $token",
-                    )
-                );
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($datas));
-                curl_setopt($curl, CURLOPT_URL, "https://simo.wablas.com/api/send-message");
-                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-                $result = curl_exec($curl);
-                curl_close($curl);
-    }
-})->purpose('Mengirimkan pesan whatsapp ke anggota yang belum mengisi jurnal');
+//     foreach ($y as $key => $value) {
+//         # code...
+//         $curl = curl_init();
+//                 $token = "dyr07JcBSmVsb1YrVBTB2A5zNKor0BZ9krv2WnQsjWHG1CRhSktdqazkfuOSY9qh";
+//                 $datas = [
+//                     'phone' => '081329146514',
+//                     'message' => $value->nama.' - Jangan lupa mengisi jurnal kegiatan harian anda pada . *https://jurnal.tilawatipusat.com*',
+//                     'secret' => false, // or true
+//                     'priority' => false, // or true
+//                 ];
+//                 curl_setopt($curl, CURLOPT_HTTPHEADER,
+//                     array(
+//                         "Authorization: $token",
+//                     )
+//                 );
+//                 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+//                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//                 curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($datas));
+//                 curl_setopt($curl, CURLOPT_URL, "https://simo.wablas.com/api/send-message");
+//                 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+//                 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+//                 $result = curl_exec($curl);
+//                 curl_close($curl);
+//     }
+// })->purpose('Mengirimkan pesan whatsapp ke anggota yang belum mengisi jurnal');
