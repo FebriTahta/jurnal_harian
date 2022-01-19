f3f6b6465738057b2bfba021695bd47a559512c6
FebriTahta <febrirtah@gmail.com>
2022-01-19 13:15:35 +0700

    oke

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('reminderjurnal')->dailyAt('13:15');
+        $schedule->command('reminderjurnal')->dailyAt('13:17');
         // $schedule->command('reminder:cron')
         //          ->everyMinute();
     }
