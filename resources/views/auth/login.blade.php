3e9dd09679392b6a93239b2df90e9c307f254d49
FebriTahta <febrirtah@gmail.com>
2022-01-20 10:07:58 +0700

    login regis

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -11,12 +11,12 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Anggota') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="name" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                                {{-- <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="anggota_Id" value="{{ old('anggota_id') }}" required autocomplete="email" autofocus> --}}
+                                <input type="text" name="username" class="form-control">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -25,11 +25,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -39,7 +39,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -51,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
@@ -70,65 +70,4 @@
         </div>
     </div>
 </div>
-@endsection --}}
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Media NF</title>
-    <link rel="stylesheet" type="text/css" href="{{asset('login_template/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('login_template/css/fontawesome-all.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('login_template/css/iofrm-style.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('login_template/css/iofrm-theme21.css')}}">
-</head>
-<body>
-    <div class="form-body without-side">
-        <div class="website-logo">
-            <a href="index.html">
-                {{-- <div class="logo">
-                    <img class="logo-size" src="{{asset('login_template/images/nf_logo.png')}}" alt="">
-                </div> --}}
-                
-            </a>
-        </div>
-        <div class="row">
-            <div class="img-holder">
-                <div class="bg"></div>
-                <div class="info-holder">
-                    <img src="{{asset('login_template/images/graphic3.svg')}}" alt="">
-                </div>
-            </div>
-            <div class="form-holder">
-                <div class="form-content">
-                    <div class="form-items">
-                        <h3>Login to account</h3>
-                        <p>Kumpulan Media Social dan Aplikasi.</p>
-                        <form method="POST" action="{{ route('login') }}">@csrf
-                            <input class="form-control" type="text" name="name" placeholder="E-mail Address" required>
-                            <input class="form-control" type="password" name="password" placeholder="Password" required>
-                            <div class="form-button">
-                                <button id="submit" type="submit" class="ibtn">Login</button> 
-                                <a class="page-links" href="{{route('register')}}">Daftar baru</a>
-                                {{-- <a href="forget21.html">Forget password?</a> --}}
-                            </div>
-                        </form>
-                        {{-- <div class="other-links">
-                            <div class="text">Belum punya akun ? </div>
-                            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a><a href="#"><i class="fab fa-google"></i>Google</a><a href="#"><i class="fab fa-linkedin-in"></i>Linkedin</a>
-                        </div>
-                        <div class="page-links">
-                            <a class="btn btn-info text-white" href="{{route('register')}}">Daftar</a>
-                        </div> --}}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-<script src="{{asset('login_template/js/jquery.min.js')}}"></script>
-<script src="{{asset('login_template/js/popper.min.js')}}"></script>
-<script src="{{asset('login_template/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('login_template/js/main.js')}}"></script>
-</body>
-</html>
+@endsection
