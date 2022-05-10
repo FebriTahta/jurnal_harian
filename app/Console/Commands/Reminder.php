c954a265bf75a2287365f49e044e4a5b75d68e86
FebriTahta <febrirtah@gmail.com>
2022-05-10 16:47:14 +0700

    broadcast new wa

--- app/Console/Commands/Reminder.php
+++ app/Console/Commands/Reminder.php
@@ -46,27 +46,35 @@ class Reminder extends Command
 
         foreach ($y as $key => $value) {
             # code...
+
             $curl = curl_init();
-                    $token = "dyr07JcBSmVsb1YrVBTB2A5zNKor0BZ9krv2WnQsjWHG1CRhSktdqazkfuOSY9qh";
-                    $datas = [
-                        'phone' => '081329146514',
-                        'message' => $value->nama.' - Jangan lupa mengisi jurnal kegiatan harian anda pada . *https://jurnal.tilawatipusat.com*',
+            $token = "ETDoFxrQu746dzPyu4V2PxH1LxW7GuYdV2fyxGtIoklIaOz3E0ymAvbSZqeamfRa";
+            $payload = [
+                "data" => [
+                    [
+                        'phone' => $value->phone,
+                        'message' => 'Yth., Sahabat '.$value->nama.' - Jangan lupa mengisi jurnal kegiatan harian anda pada . *https://jurnal.tilawatipusat.com*',
                         'secret' => false, // or true
-                        'priority' => false, // or true
-                    ];
-                    curl_setopt($curl, CURLOPT_HTTPHEADER,
-                        array(
-                            "Authorization: $token",
-                        )
-                    );
-                    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($datas));
-                    curl_setopt($curl, CURLOPT_URL, "https://simo.wablas.com/api/send-message");
-                    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-                    $result = curl_exec($curl);
-                    curl_close($curl);
+                        'retry' => false, // or true
+                        'isGroup' => false, // or true
+                    ]
+                ]
+            ];
+            curl_setopt($curl, CURLOPT_HTTPHEADER,
+                array(
+                    "Authorization: $token",
+                    "Content-Type: application/json"
+                )
+            );
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload) );
+            curl_setopt($curl, CURLOPT_URL, "https://solo.wablas.com/api/v2/send-message");
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+
+            $result = curl_exec($curl);
+            curl_close($curl);
         }
     }
 }
