5cfa92f24c720ba23d4470e6660094dbce4835fe
FebriTahta <febrirtah@gmail.com>
2022-10-19 13:30:43 +0700

    jurnal

--- app/Console/Commands/JurnalCron.php
+++ app/Console/Commands/JurnalCron.php
@@ -42,34 +42,39 @@ class JurnalCron extends Command
 
         foreach ($y as $key => $value) {
             # code...
-            set_time_limit(0);
-            $curl = curl_init();
-            $token = "ErPMCdWGNfhhYPrrGsTdTb1vLwUbIt35CQ2KlhffDobwUw8pgYX4TN5rDT4smiIc";
-            $payload = [
-                "data" => [
-                    [
-                        'phone' => $value->phone,
-                        'message' => $value->nama.' - Anda belum mengisi jurnal harian. Dimohon untuk mengisi jurnal kegiatan hari ini pada *https://jurnal.tilawatipusat.com*',
-                        'secret' => false, // or true
-                        'retry' => false, // or true
-                        'isGroup' => false, // or true
+                set_time_limit(0);
+                $curl = curl_init();
+                $token = "ErPMCdWGNfhhYPrrGsTdTb1vLwUbIt35CQ2KlhffDobwUw8pgYX4TN5rDT4smiIc";
+                $payload = [
+                    "data" => [
+                        [
+                            'phone' =>  $value->phone,
+                            'message' => $value->nama.' - Anda belum mengisi jurnal harian. Dimohon untuk mengisi jurnal kegiatan hari ini pada *https://jurnal.tilawatipusat.com*',
+
+                            'secret' => false, // or true
+                            'retry' => false, // or true
+                            'isGroup' => false, // or true
+                        ]
                     ]
-                ]    
-            ];
-                    curl_setopt($curl, CURLOPT_HTTPHEADER,
-                        array(
-                            "Authorization: $token",
-                            "Content-Type: application/json"
-                        )
-                    );
-                    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload) );
-                    curl_setopt($curl, CURLOPT_URL, "https://solo.wablas.com/api/v2/send-message");
-                    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-                    $result = curl_exec($curl);
-                    curl_close($curl);
+                
+                ];
+                
+                
+                curl_setopt($curl, CURLOPT_HTTPHEADER,
+                    array(
+                        "Authorization: $token",
+                        "Content-Type: application/json"
+                    )
+                );
+                
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload) );
+                curl_setopt($curl, CURLOPT_URL, "https://solo.wablas.com/api/v2/send-message");
+                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+                $result = curl_exec($curl);
+                curl_close($curl);
         }
     }
 }
